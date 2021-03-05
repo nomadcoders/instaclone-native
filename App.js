93a5b77e98eefdf5084bfae44653ba67e4ca312c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-05 16:12:12 +0900

    13.7 AppLoading part Two

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
@@ -11,7 +12,12 @@ export default function App() {
   const preload = () => {
     const fontsToLoad = [Ionicons.font];
     const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
-    return Promise.all(fontPromises);
+    const imagesToLoad = [
+      require("./assets/logo.png"),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
   };
   if (loading) {
     return (
