7cae24e23a79d73503d2f1d1731ee5a6bed1ed13
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-06 20:07:14 +0900

    14.2 Navigator Props

--- navigators/LoggedOutNav.js
+++ navigators/LoggedOutNav.js
@@ -8,7 +8,11 @@ const Stack = createStackNavigator();
 
 export default function LoggedOutNav() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+      }}
+    >
       <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="LogIn" component={LogIn} />
       <Stack.Screen name="CreateAccount" component={CreateAccount} />
