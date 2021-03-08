a65be3374f56321a7882bfbb4164b60c449b3f70
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-08 23:20:42 +0900

    14.16 Recap

--- apollo.js
+++ apollo.js
@@ -6,8 +6,8 @@ export const tokenVar = makeVar("");
 
 export const logUserIn = async (token) => {
   await AsyncStorage.multiSet([
-    ["token", JSON.stringify(token)],
-    ["loggedIn", JSON.stringify("yes")],
+    ["token", token],
+    ["loggedIn", "yes"],
   ]);
   isLoggedInVar(true);
   tokenVar(token);
