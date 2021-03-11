9891591e099828f139ef04cd4974924ce52236d1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-11 16:52:25 +0900

    15.3 Stack and Tabs part Two

--- components/nav/StackNavFactory.js
+++ components/nav/StackNavFactory.js
@@ -11,7 +11,16 @@ const Stack = createStackNavigator();
 
 export default function StackNavFactory({ screenName }) {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerTintColor: "white",
+        headerStyle: {
+          shadowColor: "rgba(255, 255, 255, 0.3)",
+          backgroundColor: "black",
+        },
+      }}
+    >
       {screenName === "Feed" ? (
         <Stack.Screen name={"Feed"} component={Feed} />
       ) : null}
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function Feed() {
+export default function Feed({ navigation }) {
   return (
     <View
       style={{
@@ -11,7 +11,9 @@ export default function Feed() {
         justifyContent: "center",
       }}
     >
-      <Text style={{ color: "white" }}>Feed</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Photo")}>
+        <Text style={{ color: "white" }}>Photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
