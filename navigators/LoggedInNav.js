93ae021f3e42f72e79163a517cdb2e17104824d0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-12 17:52:17 +0900

    16.4 Search part Two

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import TabIcon from "../components/nav/TabIcon";
 import SharedStackNav from "./SharedStackNav";
+import useMe from "../hooks/useMe";
 
 const Tabs = createBottomTabNavigator();
 
 export default function LoggedInNav() {
+  const { data } = useMe();
   return (
     <Tabs.Navigator
       tabBarOptions={{
@@ -60,9 +62,20 @@ export default function LoggedInNav() {
       <Tabs.Screen
         name="Me"
         options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TabIcon iconName={"person"} color={color} focused={focused} />
-          ),
+          tabBarIcon: ({ focused, color, size }) =>
+            data?.me?.avatar ? (
+              <Image
+                source={{ uri: data.me.avatar }}
+                style={{
+                  height: 20,
+                  width: 20,
+                  borderRadius: 10,
+                  ...(focused && { borderColor: "white", borderWidth: 1 }),
+                }}
+              />
+            ) : (
+              <TabIcon iconName={"person"} color={color} focused={focused} />
+            ),
         }}
       >
         {() => <SharedStackNav screenName="Me" />}
--- screens/Search.js
+++ screens/Search.js
@@ -1,13 +1,24 @@
+import { gql, useLazyQuery } from "@apollo/client";
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { Text, TextInput, View } from "react-native";
 import styled from "styled-components/native";
 import DismissKeyboard from "../components/DismissKeyboard";
 
+const SEARCH_PHOTOS = gql`
+  query searchPhotos($keyword: String!) {
+    searchPhotos(keyword: $keyword) {
+      id
+      file
+    }
+  }
+`;
+
 const Input = styled.TextInput``;
 
 export default function Search({ navigation }) {
   const { setValue, register } = useForm();
+  const [startQueryFn, { loading, data }] = useLazyQuery(SEARCH_PHOTOS);
   const SearchBox = () => (
     <TextInput
       style={{ backgroundColor: "white" }}
