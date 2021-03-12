45e577b589936e9011b29d01d107fd564115b66b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-12 21:52:44 +0900

    17.5 Select Photo part Four

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   useWindowDimensions,
 } from "react-native";
+import { colors } from "../colors";
 
 const Container = styled.View`
   flex: 1;
@@ -31,7 +32,14 @@ const IconContainer = styled.View`
   right: 0px;
 `;
 
-export default function SelectPhoto() {
+const HeaderRightText = styled.Text`
+  color: ${colors.blue};
+  font-size: 16px;
+  font-weight: 600;
+  margin-right: 7px;
+`;
+
+export default function SelectPhoto({ navigation }) {
   const [ok, setOk] = useState(false);
   const [photos, setPhotos] = useState([]);
   const [chosenPhoto, setChosenPhoto] = useState("");
@@ -56,8 +64,18 @@ export default function SelectPhoto() {
       getPhotos();
     }
   };
+  const HeaderRight = () => (
+    <TouchableOpacity>
+      <HeaderRightText>Next</HeaderRightText>
+    </TouchableOpacity>
+  );
   useEffect(() => {
     getPermissions();
+  }, []);
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: HeaderRight,
+    });
   }, []);
   const numColumns = 4;
   const { width } = useWindowDimensions();
@@ -71,7 +89,11 @@ export default function SelectPhoto() {
         style={{ width: width / numColumns, height: 100 }}
       />
       <IconContainer>
-        <Ionicons name="checkmark-circle" size={18} color="white" />
+        <Ionicons
+          name="checkmark-circle"
+          size={18}
+          color={photo.uri === chosenPhoto ? colors.blue : "white"}
+        />
       </IconContainer>
     </ImageContainer>
   );
