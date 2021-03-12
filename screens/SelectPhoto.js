27fc6300e1c319afa440f18225dc650e218e2199
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-12 21:19:30 +0900

    17.3 Select Photo part Two

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -17,10 +18,36 @@ const Bottom = styled.View`
 `;
 
 export default function SelectPhoto() {
+  const [ok, setOk] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const getPhotos = async () => {
+    if (ok) {
+      const { assets: photos } = await MediaLibrary.getAssetsAsync();
+      setPhotos(photos);
+    }
+  };
+  const getPermissions = async () => {
+    const {
+      accessPrivileges,
+      canAskAgain,
+    } = await MediaLibrary.getPermissionsAsync();
+    if (accessPrivileges === "none" && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
+      if (accessPrivileges !== "none") {
+        setOk(true);
+      }
+    } else if (accessPrivileges !== "none") {
+      setOk(true);
+    }
+  };
+  useEffect(() => {
+    getPermissions();
+    getPhotos();
+  }, []);
   return (
     <Container>
       <Top />
-      <Bottom />
+      <Bottom></Bottom>
     </Container>
   );
 }
