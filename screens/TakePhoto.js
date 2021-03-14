ffceac897c901baf29a1f065f54a2e8af9d4f132
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-14 17:26:41 +0900

    17.9 Take Photo part Three

--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -1,5 +1,5 @@
 import { Camera } from "expo-camera";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { StatusBar, TouchableOpacity } from "react-native";
 import Slider from "@react-native-community/slider";
@@ -44,6 +44,8 @@ const CloseButton = styled.TouchableOpacity`
 `;
 
 export default function TakePhoto({ navigation }) {
+  const camera = useRef();
+  const [cameraReady, setCameraReady] = useState(false);
   const [ok, setOk] = useState(false);
   const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
   const [zoom, setZoom] = useState(0);
@@ -74,6 +76,16 @@ export default function TakePhoto({ navigation }) {
       setFlashMode(Camera.Constants.FlashMode.off);
     }
   };
+  const onCameraReady = () => setCameraReady(true);
+  const takePhoto = async () => {
+    if (camera.current && cameraReady) {
+      const photo = await camera.current.takePictureAsync({
+        quality: 1,
+        exif: true,
+      });
+      console.log(photo);
+    }
+  };
   return (
     <Container>
       <StatusBar hidden={true} />
@@ -82,6 +94,8 @@ export default function TakePhoto({ navigation }) {
         style={{ flex: 1 }}
         zoom={zoom}
         flashMode={flashMode}
+        ref={camera}
+        onCameraReady={onCameraReady}
       >
         <CloseButton onPress={() => navigation.navigate("Tabs")}>
           <Ionicons name="close" color="white" size={30} />
@@ -99,7 +113,7 @@ export default function TakePhoto({ navigation }) {
           />
         </SliderContainer>
         <ButtonsContainer>
-          <TakePhotoBtn />
+          <TakePhotoBtn onPress={takePhoto} />
           <ActionsContainer>
             <TouchableOpacity
               onPress={onFlashChange}
