0a4b5ae254dac821d564e53748566fe7b9dcfc1f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-03-15 20:20:54 +0900

    18.2 Rooms Screen part Two

--- screens/Rooms.js
+++ screens/Rooms.js
@@ -1,9 +1,11 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, View } from "react-native";
 import styled from "styled-components/native";
+import { colors } from "../colors";
 import ScreenLayout from "../components/ScreenLayout";
 import { ROOM_FRAGMENT } from "../fragments";
+import useMe from "../hooks/useMe";
 
 const SEE_ROOMS_QUERY = gql`
   query seeRooms {
@@ -14,27 +16,78 @@ const SEE_ROOMS_QUERY = gql`
   ${ROOM_FRAGMENT}
 `;
 
-const RoomContainer = styled.View`
-  background-color: black;
+const RoomContainer = styled.TouchableOpacity`
+  width: 100%;
+  padding: 15px 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `;
-const RoomText = styled.Text`
+const Column = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  margin-right: 20px;
+`;
+const Data = styled.View``;
+const UnreadDot = styled.View`
+  width: 10px;
+  border-radius: 5px;
+  height: 10px;
+  background-color: ${colors.blue};
+`;
+const Username = styled.Text`
   color: white;
+  font-weight: 600;
+  font-size: 16px;
+`;
+const UnreadText = styled.Text`
+  color: white;
+  margin-top: 2px;
+  font-weight: 500;
 `;
 
 export default function Rooms() {
   const { data, loading } = useQuery(SEE_ROOMS_QUERY);
-  const renderItem = ({ item: room }) => (
-    <RoomContainer>
-      <RoomText>
-        {room.unreadTotal === "0"
-          ? "Name of the other person"
-          : `${room.unreadTotal} messages.`}
-      </RoomText>
-    </RoomContainer>
-  );
+  const { data: meData } = useMe();
+  const renderItem = ({ item: room }) => {
+    const notMe = room.users.find(
+      (user) => user.username !== meData?.me?.username
+    );
+    return (
+      <RoomContainer>
+        <Column>
+          <Avatar source={{ uri: notMe.avatar }} />
+          <Data>
+            <Username>{notMe.username}</Username>
+            <UnreadText>
+              {room.unreadTotal} unread{" "}
+              {room.unreadTotal === 1 ? "message" : "messages"}
+            </UnreadText>
+          </Data>
+        </Column>
+        <Column>{room.unreadTotal !== 0 ? <UnreadDot /> : null}</Column>
+      </RoomContainer>
+    );
+  };
   return (
     <ScreenLayout loading={loading}>
       <FlatList
+        ItemSeparatorComponent={
+          <View
+            style={{
+              width: "100%",
+              height: 1,
+              backgroundColor: "rgba(255, 255, 255, 0.2)",
+            }}
+          ></View>
+        }
+        style={{ width: "100%" }}
         data={data?.seeRooms}
         keyExtractor={(room) => "" + room.id}
         renderItem={renderItem}
